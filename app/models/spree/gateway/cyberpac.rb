746b0f3b367ff3760cf6486697d71f92b7ca081c
Ruben Sierra <maguilag@gmail.com>
2015-10-13 19:04:35 +0200

    Add Spree Cyberpac gateway

---
+++ app/models/spree/gateway/cyberpac.rb
@@ -0,0 +1,11 @@
+module Spree
+  class Gateway::Cyberpac < Gateway
+    preference :merchant_code, :string, default: '003239696'
+    preference :secret_key, :string, default: 'qwertyasdf0123456789'
+    preference :terminal, :string, default: '1'
+
+    def provider_class
+      ::ActiveMerchant::Billing::Gateways::CyberpacGateway
+    end
+  end
+end
--- lib/spree_cyberpac_payment/engine.rb
+++ lib/spree_cyberpac_payment/engine.rb
@@ -16,5 +16,9 @@ module SpreeCyberpacPayment
     end
 
     config.to_prepare &method(:activate).to_proc
+
+    initializer "spree.cyberpac_payment.payment_methods", after: "spree.register.payment_methods" do |app|
+      app.config.spree.payment_methods << Spree::Gateway::Cyberpac
+    end
   end
 end
