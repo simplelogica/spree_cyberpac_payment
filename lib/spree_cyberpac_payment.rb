aedab8453dc919d0da23d37519f95ecf20a1b747
Ruben Sierra <maguilag@gmail.com>
2015-10-13 19:03:14 +0200

    Add Cyberpac gateway
    Copied from https://github.com/geronimod/active_merchant.

--- lib/spree_cyberpac_payment.rb
+++ lib/spree_cyberpac_payment.rb
@@ -1,2 +1,4 @@
 require 'spree_core'
 require 'spree_cyberpac_payment/engine'
+require 'active_merchant/billing/gateways/cyberpac/cyberpac'
+require 'active_merchant/billing/gateways/cyberpac/cyberpac_response'
