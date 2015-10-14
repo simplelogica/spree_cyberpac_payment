74ef6aa91096c26dd8fde6a5ddca7305c0f1b626
Ruben Sierra <maguilag@gmail.com>
2015-10-14 19:37:43 +0200

    Add merchant name preference

--- app/models/spree/gateway/cyberpac.rb
+++ app/models/spree/gateway/cyberpac.rb
@@ -1,5 +1,6 @@
 module Spree
   class Gateway::Cyberpac < Gateway
+    preference :merchant_name, :string
     preference :merchant_code, :string, default: '003239696'
     preference :secret_key, :string, default: 'qwertyasdf0123456789'
     preference :terminal, :string, default: '1'
