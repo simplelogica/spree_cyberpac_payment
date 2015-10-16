2879becb279727abc01ecd503a8ade9ae02314f9
Ruben Sierra <maguilag@gmail.com>
2015-10-16 10:38:50 +0200

    Gateway configuration for simple payment request

--- app/models/spree/gateway/cyberpac.rb
+++ app/models/spree/gateway/cyberpac.rb
@@ -12,5 +12,22 @@ module Spree
     def method_type
       'cyberpac'
     end
+
+    def auto_capture?
+      true
+    end
+
+    # We Fake a source with the payment method that don't responds to brand
+    # becouse que creadit card is managed in cyberpac, so we always support
+    # the source in the gateway
+    def supports?(source)
+      true
+    end
+
+    def purchase(amount, checkout, gateway_options={})
+      # The payment is confirmed in the notify call, so we fake a success
+      # for the auto capture calls
+      ActiveMerchant::Billing::CyberpacResponse.new(true, 'purchase', {})
+    end
   end
 end
