eb8c1de0d661e11d013f32d0d2a5aaf38ad2fe3f
Ruben Sierra <maguilag@gmail.com>
2015-10-19 10:34:25 +0200

    Add version module

--- lib/spree_cyberpac_payment.rb
+++ lib/spree_cyberpac_payment.rb
@@ -1,4 +1,5 @@
 require 'spree_core'
 require 'spree_cyberpac_payment/engine'
+require 'spree_cyberpac_payment/version'
 require 'active_merchant/billing/gateways/cyberpac/cyberpac'
 require 'active_merchant/billing/gateways/cyberpac/cyberpac_response'
