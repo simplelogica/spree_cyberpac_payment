6a772995396e10313756a078713745bf66535dad
Ruben Sierra <maguilag@gmail.com>
2016-11-22 11:51:00 +0100

    Create always a different order number

--- app/decorators/models/spree/order_decorator.rb
+++ app/decorators/models/spree/order_decorator.rb
@@ -1,8 +1,15 @@
 Spree::Order.class_eval do
   def cyberpac_number
-    payment_number = "#{number}"
-    payment_number += "-#{payments.count}" if payments.any?
-    payment_number
+    # Send always a different order number to avoid problems with retrys
+    # The provider doesn't accept request with duplicate order numbers
+    # The number can't be more tha 12 characters and ordar numbers are 9
+    # characters long
+    "#{number}-#{random_chars(2)}"
+  end
+
+  def random_chars n=1
+    chars = [*'A'..'Z',*0..9].sample(n)
+    chars.join('')
   end
 
   def cyberpac_purchase_data
